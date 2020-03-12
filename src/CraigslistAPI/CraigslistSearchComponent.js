b3bab02e34872adaa994f7dd8cc870d9f45e826e
kevinliang43 <kevinliang43@gmail.com>
2020-03-12 03:00:57 -0400

    Added city parameter to the search component.

--- src/CraigslistAPI/CraigslistSearchComponent.js
+++ src/CraigslistAPI/CraigslistSearchComponent.js
@@ -1,32 +1,39 @@
 import React from "react";
-import {Client} from "node-craigslist"
 import ListComponent from "../components/ListComponent"
 import {searchListings} from "../services/CraigslistService"
+import {defaultCity} from "../constants"
 
 
 export default class CraigslistSearchComponent extends React.Component {
 
     componentDidMount() {
         let searchQuery = this.props.match.params.searchPost;
-        if(searchQuery) {
-            this.getListings(searchQuery)
+        let searchCity = this.props.match.params.city;
+        if (searchCity) {
+            this.setState({city: searchCity})
+        }
+        if (searchQuery) {
+            this.getListings(searchCity, searchQuery)
         }
     }
 
     state = {
         listings : [],
         searchQuery: '',
-        city: 'boston'
+        city: ''
     }
 
-    getListings = (searchQuery) => {
-        this.props.history.push(`/search/${searchQuery}`)
-        searchListings(this.state.city, searchQuery, 20)
+    getListings = (searchCity, searchQuery) => {
+        searchCity = this.normalizeCity(searchCity);
+        this.props.history.push(`/search/${searchCity}/${searchQuery}`)
+        searchListings(searchCity, searchQuery, 20)
             .then(results => this.setState({
                 listings : results
             }))
+    }
 
-
+    normalizeCity = (cityString) => {
+        return cityString.toLowerCase().replace(/\s/g, '')
     }
 
     render() {
@@ -35,17 +42,23 @@ export default class CraigslistSearchComponent extends React.Component {
             <h2>Search Listings</h2>
             <input className={`form-control`}
                     onChange={e => this.setState({searchQuery: e.target.value})}
-                    value={this.state.searchQuery}/>
-            <button className={`btn btn-success btn-block`}
-                    onClick={() => this.getListings(this.state.searchQuery)}>
-                        Search For Listings
-            </button>
+                    value={this.state.searchQuery}
+                    placeHolder={`Search for Listings`}/>
+            <input className={`form-control`}
+                    onChange={e => this.setState({city: e.target.value})}
+                    value={this.state.city}
+                    placeHolder={`City to search for listings in`}/>
 
-            <ul className={`list-group`}>
+            <button className={`btn btn-success btn-block`}
+                    onClick={() => this.getListings(this.state.city, this.state.searchQuery)}>
+                        Search
+            </button>
+            <ul className={`list-group mt-2`}>
                 {this.state.listings.map((listing, idx) =>
                         <ListComponent
                             idx={idx}
                             listing={listing}
+                            city={this.state.city}
                         />
                 )
 
