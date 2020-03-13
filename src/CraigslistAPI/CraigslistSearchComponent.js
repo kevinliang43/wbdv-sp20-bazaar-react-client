95f2b39dde6bf5f975299608668f77e42bbc22a6
kevinliang43 <kevinliang43@gmail.com>
2020-03-13 02:24:47 -0400

    Added buttons for list/grid view. Added labels for the form inputs.

--- src/CraigslistAPI/CraigslistSearchComponent.js
+++ src/CraigslistAPI/CraigslistSearchComponent.js
@@ -41,48 +41,61 @@ export default class CraigslistSearchComponent extends React.Component {
 
     render() {
         return (
-            <div>
-            <h2>Search Listings</h2>
-            {this.state.view === 'LIST' &&
-                <button onClick={() => this.setState({view: 'GRID'})}>Grid View</button>
-            }
-            {this.state.view === 'GRID' &&
-                <button onClick={() => this.setState({view: 'LIST'})}>List View</button>
-            }
-            <input className={`form-control`}
-                    onChange={e => this.setState({searchQuery: e.target.value})}
-                    value={this.state.searchQuery}
-                    placeHolder={`Search for Listings`}/>
-            <input className={`form-control`}
-                    onChange={e => this.setState({city: e.target.value})}
-                    value={this.state.city}
-                    placeHolder={`City to search for listings in`}/>
+            <div className="mt-4">
+            <h2>Search Listings
+                {this.state.view === 'LIST' &&
+                    <button className="btn btn-success float-right" onClick={() => this.setState({view: 'GRID'})}>
+                        <i className="fa fa-th"/>
+                    </button>
+                }
+                {this.state.view === 'GRID' &&
+                    <button className="btn btn-success float-right" onClick={() => this.setState({view: 'LIST'})}>
+                    <i className="fa fa-list"></i>
+                    </button>
+                }
+            </h2>
+
+            <div class="form-group">
+                <label for="searchQuery" class="col-form-label">Search Bazaar</label>
+                <input className={`form-control`}
+                        onChange={e => this.setState({searchQuery: e.target.value})}
+                        value={this.state.searchQuery}
+                        placeHolder={`Search for Listings`}/>
+
+                <label for="searchCity" class="col-form-label">City</label>
+                <input className={`form-control`}
+                        onChange={e => this.setState({city: e.target.value})}
+                        value={this.state.city}
+                        placeHolder={`City to search for listings in`}/>
+            </div>
 
             <button className={`btn btn-success btn-block`}
                     onClick={() => this.getListings(this.state.city, this.state.searchQuery)}>
                         Search
             </button>
-                {this.state.view === 'LIST' && 
 
-                    <ul className={`list-group mt-2`}>
-                        {this.state.listings.map((listing, idx) =>
-                                <ListingRowComponent
-                                    idx={idx}
-                                    listing={listing}
-                                    city={this.state.city}/>
-                        )}
-                    </ul>
-                }
-                {this.state.view === 'GRID' && 
-                    <div className="card-deck row m-1 no-gutters">
-                        {this.state.listings.map((listing, idx) =>
-                        <ListingCardComponent
-                            idx={idx}
-                            listing={listing}
-                            city={this.state.city}/>
+            {this.state.view === 'LIST' && 
+                <ul className={`list-group mt-2`}>
+                    {this.state.listings.map((listing, idx) =>
+                            <ListingRowComponent
+                                idx={idx}
+                                listing={listing}
+                                city={this.state.city}/>
                     )}
-                    </div>
-                }
+                </ul>
+            }
+
+            {this.state.view === 'GRID' && 
+                <div className="card-deck row m-1 no-gutters">
+                    {this.state.listings.map((listing, idx) =>
+                    <ListingCardComponent
+                        idx={idx}
+                        listing={listing}
+                        city={this.state.city}/>
+                    )}
+                </div>
+            }
+            
         </div>
         )
     }
