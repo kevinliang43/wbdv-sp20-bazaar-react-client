a85c428b4a81bc1fff1aa410445786ff4c21620d
kevinliang43 <kevinliang43@gmail.com>
2020-03-28 01:25:28 -0400

    Updated routing so that home is mapped to '/' and search is '/search'

--- src/containers/BazaarContainer.js
+++ src/containers/BazaarContainer.js
@@ -3,6 +3,7 @@ import CraigslistComponent from "../CraigslistAPI/CraigslistComponent"
 import LoginComponent from "../components/login/LoginComponent"
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import RegisterComponent from "../components/register/RegisterComponent";
+import HomeComponent from "../components/home/HomeComponent";
 
 export default class BazaarContainer extends React.Component {
 
@@ -11,6 +12,11 @@ export default class BazaarContainer extends React.Component {
             <Router>
                 <Route
                     path={`/`}
+                    exact={true}
+                    component={HomeComponent}/>
+
+                <Route
+                    path={`/search`}
                     exact={true}
                     component={CraigslistComponent}/>
 
