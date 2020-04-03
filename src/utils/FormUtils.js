19510c7bd449f181e4bf1a4505cb95870a3b8c3e
kevinliang43 <kevinliang43@gmail.com>
2020-04-03 00:13:32 -0400

    Added utility for checking email validity

--- src/utils/FormUtils.js
+++ src/utils/FormUtils.js
@@ -15,4 +15,10 @@ export const checkForEmptyFields = (form) => {
 
 }
 
+export const checkEmailFormat = (email) => {
+    // Credit: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 export default {checkParamsMatch, checkForEmptyFields}
