eb7d5844ca4c0fb1c1774acc718fd453f6055ea4
kevinliang43 <kevinliang43@gmail.com>
2020-04-02 22:03:20 -0400

    Added URL endpoint for Users.

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
 export const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
 export const defaultCity = 'boston'
+
+export const API_USER_URL = "https://wbdv-sp20-bazaar-server-java.herokuapp.com/api/users"
