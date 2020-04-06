2104a7f880679106a98573d14a199a0b24b77979
kevinliang43 <kevinliang43@gmail.com>
2020-04-06 03:02:06 -0400

    Added URL for retreiving craigslist locations.

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,4 @@ export const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
 export const defaultCity = 'boston'
 
 export const API_USER_URL = "https://wbdv-sp20-bazaar-server-java.herokuapp.com/api/users"
+export const CRAIGSLIST_LOCATIONS_URL = "https://www.craigslist.org/about/sites"
