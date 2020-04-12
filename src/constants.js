a967cd0a4c1aafe652bb42b54bbd58e917ad9ccf
kevinliang43 <kevinliang43@gmail.com>
2020-04-12 19:55:05 -0400

    Updated proxyURL.

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 //export const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-export const proxyUrl = 'https://bazaar-proxy-server.herokuapp.com/'
+export const proxyUrl = 'https://secret-ocean-49799.herokuapp.com/' //https://github.com/messier31/cors-proxy-server server
+//export const proxyUrl = 'https://bp-server-1.herokuapp.com/'
 export const defaultCity = 'boston'
 
 export const API_USER_URL = "https://wbdv-sp20-bazaar-server-java.herokuapp.com/api/users"
