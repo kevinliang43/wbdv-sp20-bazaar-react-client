2a0c15e4826825580a816576c5ba638e69a2bea2
kevinliang43 <kevinliang43@gmail.com>
2020-04-19 04:18:09 -0400

    Reverted URL service endpoints.

--- src/constants.js
+++ src/constants.js
@@ -2,8 +2,9 @@ export const proxyUrl = 'https://secret-ocean-49799.herokuapp.com/'
 //https://github.com/messier31/cors-proxy-server Updated Proxy Server Github reference
 export const defaultCity = 'boston'
 
-// export const API_USER_URL = "https://wbdv-sp20-bazaar-server-java.herokuapp.com/api/users"
-// export const API_BASE_URL = "https://wbdv-sp20-bazaar-server-java.herokuapp.com"
-export const API_USER_URL = "https://boiling-mesa-99660.herokuapp.com/api/users"
-export const API_BASE_URL = "https://boiling-mesa-99660.herokuapp.com"
+export const API_USER_URL = "https://wbdv-sp20-bazaar-server-java.herokuapp.com/api/users"
+export const API_BASE_URL = "https://wbdv-sp20-bazaar-server-java.herokuapp.com"
+// Staging Instance
+// export const API_USER_URL = "https://boiling-mesa-99660.herokuapp.com/api/users"
+// export const API_BASE_URL = "https://boiling-mesa-99660.herokuapp.com"
 export const CRAIGSLIST_LOCATIONS_URL = "https://www.craigslist.org/about/sites"
