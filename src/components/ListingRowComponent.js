3e66efb7652a43a86063fe937356e66311bfe1ca
kevinliang43 <kevinliang43@gmail.com>
2020-04-20 03:48:19 -0400

    Updated listingRowComponent to render differently depending on craigslist/bazaar post.

--- src/components/ListingRowComponent.js
+++ src/components/ListingRowComponent.js
@@ -10,15 +10,28 @@ class ListingRowComponent extends React.Component {
             <li className="list-group-item" key={this.props.idx}>
                 <span className="row">
                     <div className="col-3">
-                        <img className="row-img"
+                        {this.props.type === "craigslist" &&
+                            <img className="row-img"
                             src={`https://images.craigslist.org/${this.props.listing.imagePaths[0]}_300x300.jpg`}
                             alt='No Images Available'></img>
+                        }
+                        {this.props.type === "bazaar" &&
+                            <img className="row-img"
+                            src={this.props.listing.imageUrl}
+                            alt='No Images Available'></img>
+                        }
+
                     </div>
                     <div className="col-9">
                         <div className ="d-flex w-100 justify-content-between">
-                            <Link to={`/posts/${this.props.city}/${this.props.listing.pid}`}>
+                            {this.props.type === "craigslist" && <Link to={`/posts/${this.props.city}/${this.props.listing.pid}`}>
                                 <h5>{this.props.listing.title}</h5>
-                            </Link>
+                            </Link>}
+
+                            {this.props.type === "bazaar" && <Link to={`/listings/${this.props.listing.id}`}>
+                                <h5>{this.props.listing.title}</h5>
+                            </Link>}
+
                             <small>Posted: {parseDate(this.props.listing.date)}</small>
                         </div>
                         <p className="mb-1">
